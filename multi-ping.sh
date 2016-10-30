f9ea9819c9121954d9d161af064634b7dca52dc1
LinuxFrench <linuxfrench@gmail.com>
2016-10-30 12:35:51 +0100

    Create multi-ping.sh

---
+++ multi-ping.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+CODE_RETOUR=0
+
+for host in "$@"
+do
+        if ping -c1 -w1 "${host}" > /dev/null 2>&1
+        then
+                echo "${host} OK"
+        else
+                echo "${host} inacessible"
+                CODE_RETURN=1
+        fi
+done
+
+exit ${CODE_RETOUR}
